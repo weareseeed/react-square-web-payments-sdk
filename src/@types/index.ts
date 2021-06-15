50088001b346de091fb41197f3bc418813a536c2
Daniel Esteves <daniel@seeed.us>
2021-06-14 23:20:37 -0400

    fix(lint): linter for files

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,27 +1,31 @@
 // Vendor Modules
 import { MethodType, Payments } from '@square/web-payments-sdk-types';
 
-export type Methods = keyof { [M in keyof typeof MethodType as `${typeof MethodType[M]}`]: M };
+type ValueOf<T> = T[keyof T];
+export type Methods = ValueOf<MethodType>;
 
 export type ActionMethodReducer = {
-  type: "CHANGE_STATE",
-  payload: Record<MethodType, "loading" | "ready" | "unavailable">[]
-}
+  type: 'CHANGE_STATE' | Methods;
+  payload: Record<MethodType, 'loading' | 'ready' | 'unavailable'>[];
+};
 
 export type MethodsSupported = {
-  ach?: boolean
-  applePay?: boolean
-  card?: boolean
-  cashApp?: boolean
-  googlePay?: boolean
-  giftCard?: boolean
-}
+  ach?: boolean;
+  applePay?: boolean;
+  card?: boolean;
+  cashApp?: boolean;
+  googlePay?: boolean;
+  giftCard?: boolean;
+};
 
-export type InitialStateMethods = Record<"ach" | "applePay" | "card" | "cashApp" | "googlePay" | "giftCard", "loading" | "ready" | "unavailable">
+export type InitialStateMethods = Record<
+  'ach' | 'applePay' | 'card' | 'cashApp' | 'googlePay' | 'giftCard',
+  'loading' | 'ready' | 'unavailable'
+>;
 
 export interface FormContextInterface extends InitialStateMethods {
   /** Unique form ID */
   formId?: string;
-  payments?: Payments
-  dispatchMethods: React.Dispatch<ActionMethodReducer>
+  payments?: Payments;
+  dispatchMethods: React.Dispatch<ActionMethodReducer>;
 }
