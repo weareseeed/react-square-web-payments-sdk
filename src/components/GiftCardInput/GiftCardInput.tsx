b1024ab851b9f0e13ae84978754caefc51291bad
Daniel Esteves <daniel@seeed.us>
2021-06-16 13:11:32 -0400

    feat(method): add gift card payment method

---
+++ src/components/GiftCardInput/GiftCardInput.tsx
@@ -0,0 +1,73 @@
+// Vendor Modules
+import { CSSObject } from '@emotion/styled';
+import { document } from 'browser-monads-ts';
+import * as React from 'react';
+import { useEvent } from 'react-use';
+import type { GiftCard, GiftCardOptions } from '@square/web-sdk';
+
+// Internals
+import { useForm } from '../../hooks';
+import { LoadingCard, PayButton } from './styles';
+
+interface Props extends GiftCardOptions {
+  overrideStyles?: CSSObject | undefined;
+}
+
+export const CreditCardInput = ({
+  overrideStyles,
+  ...props
+}: Props): JSX.Element => {
+  const [gCard, setGCard] = React.useState<GiftCard | undefined>(
+    () => undefined
+  );
+  const { payments } = useForm();
+
+  const handlePayment = async () => {
+    try {
+      const result = await gCard?.tokenize();
+
+      console.log(result);
+      // TODO: use result.token as source_id in /v2/payments API call
+    } catch (ex) {
+      console.error(ex);
+    }
+  };
+
+  const start = async () => {
+    const gCard = await payments.giftCard(props).then((res) => {
+      setGCard(res);
+
+      return res;
+    });
+
+    await gCard?.attach('#gift-card-container');
+  };
+
+  React.useEffect(() => {
+    start();
+  }, [payments]);
+
+  useEvent(
+    'click',
+    handlePayment,
+    document.getElementById('pay-with-gift-card')
+  );
+
+  return (
+    <>
+      <div id="gift-card-container" style={{ minHeight: 89 }}>
+        {!gCard && <LoadingCard />}
+      </div>
+
+      <PayButton
+        id="pay-with-gift-card"
+        type="button"
+        overrideStyles={overrideStyles}
+      >
+        Pay with Gift Card
+      </PayButton>
+    </>
+  );
+};
+
+export default CreditCardInput;
---
+++ src/components/GiftCardInput/styles.ts
@@ -0,0 +1,41 @@
+// Vendor Modules
+import { keyframes } from '@emotion/react';
+import styled, { CSSObject } from '@emotion/styled';
+
+const pulse = keyframes`
+  0%, 100% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: .5;
+  }
+`;
+
+export const LoadingCard = styled.div({
+  animation: `${pulse} 2s cubic-bezier(0.4, 0, 0.6, 1) infinite`,
+  background: '#F3F4F6',
+  borderRadius: 6,
+  height: 50,
+  marginBottom: 39,
+  position: 'relative',
+});
+
+export const PayButton = styled.button<{
+  overrideStyles?: CSSObject;
+}>((props) => ({
+  background: 'rgba(0, 0, 0, 1)',
+  borderRadius: 6,
+  border: 'none',
+  color: 'white',
+  cursor: 'pointer',
+  display: 'block',
+  fontSize: 16,
+  lineHeight: `18px`,
+  padding: 16,
+  width: '100%',
+  '&:hover': {
+    background: 'rgba(0, 0, 0, 0.85)',
+  },
+  ...props.overrideStyles,
+}));
