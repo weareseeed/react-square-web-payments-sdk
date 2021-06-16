c6b6af3dd0659d81c258b235f193ef9fd4f61fca
Daniel Esteves <daniel@seeed.us>
2021-06-16 17:42:21 -0400

    fix(docs): add tsconfig compatibility to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
@@ -5,5 +7,12 @@ module.exports = {
   typescript: {
     check: true, // type-check stories during Storybook build
     //reactDocgen: 'none',
+  },
+  webpackFinal: (config) => {
+    if (config.resolve.plugins) {
+      config.resolve.plugins.push(new TsconfigPathsPlugin());
+    } else {
+      config.resolve.plugins = [new TsconfigPathsPlugin()];
+    }
   },
 };
