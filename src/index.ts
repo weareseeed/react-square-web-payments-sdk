3abdbd126b3e3d9d0ab72003350183d943935969
Daniel Esteves <daniel@seeed.us>
2021-06-17 17:28:12 -0400

    fix(import): import export components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-export * from '@/components/AchPayButton';
-export * from '@/components/ApplePayButton';
-export * from '@/components/CreditCardInput';
-export * from '@/components/GiftCardInput';
-export * from '@/components/GooglePayButton';
-export * from '@/components/LoadingForm';
-export * from '@/components/NoLocationIdOrAppId';
-export * from '@/components/Separator';
-export * from '@/components/SquarePaymentsForm';
+import { SquarePaymentsForm } from './components/SquarePaymentsForm';
+
+export * from './components/AchPayButton';
+export * from './components/ApplePayButton';
+export * from './components/CreditCardInput';
+export * from './components/GiftCardInput';
+export * from './components/GooglePayButton';
+export * from './components/LoadingForm';
+export * from './components/NoLocationIdOrAppId';
+export * from './components/Separator';
+export * from './components/SquarePaymentsForm';
+
+export default SquarePaymentsForm;
