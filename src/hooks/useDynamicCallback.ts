2f6ec8afb24a883f707d78425045eeef52f96ea9
Daniel Esteves <daniel@seeed.us>
2021-06-18 15:19:38 -0400

    fix(hook): useIsomorphicLayoutEffect for ssr

--- src/hooks/useDynamicCallback.ts
+++ src/hooks/useDynamicCallback.ts
@@ -1,10 +1,18 @@
 // Vendor Modules
 import * as React from 'react';
 
+// React currently throws a warning when using useLayoutEffect on the server.
+// To get around it, we can conditionally useEffect on the server (no-op) and
+// useLayoutEffect in the browser. We need useLayoutEffect because we want
+// `connect` to perform sync updates to a ref to save the latest props after
+// a render is actually committed to the DOM.
+const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
+
 export const useDynamicCallback = (callback: any) => {
   const ref = React.useRef(callback);
 
-  React.useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     ref.current = callback;
   }, [callback]);
 
