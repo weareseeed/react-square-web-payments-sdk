44264a98673e2ab1f0814ccbaf983f216220c43b
Daniel Esteves <daniel@seeed.us>
2021-06-18 15:09:50 -0400

    bump(chore): removing dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,8 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  },
-  webpackFinal: (config) => {
-    if (config.resolve.plugins) {
-      config.resolve.plugins.push(new TsconfigPathsPlugin());
-    } else {
-      config.resolve.plugins = [new TsconfigPathsPlugin()];
-    }
-
-    return config;
   },
 };
