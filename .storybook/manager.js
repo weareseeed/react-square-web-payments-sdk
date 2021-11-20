bf13b7aa12a04e678e9113900323f2bd7f33dcce
Daniel Esteves <daniel@seeed.us>
2021-11-19 21:01:52 -0400

    feat(docs): update docs with absolute imports

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,8 @@ import { themes } from '@storybook/theming'
 addons.setConfig({
   theme: {
     ...themes.light,
-    brandImage: './logo.png',
+    brandImage:
+      'https://github.com/weareseeed/react-square-web-payments-sdk/blob/main/.github/logo.png?raw=true',
     brandTitle: 'react-square-web-payments-sdk',
     fontBase:
       '"Square Market", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
