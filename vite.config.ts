29b46534a63678e9f559d0f3d990a39e7e9d689c
Daniel Esteves <me+github@danestves.com>
2022-04-26 18:09:41 -0400

    feat: tree shaking working with rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // Internals
 import { peerDependencies, dependencies } from './package.json';
 
+const externalPackages = [
+  // We need to build @square/web-sdk with the package to avoid problems with ES Modules in bundlers like Next.js
+  ...Object.keys(dependencies).filter((name) => name !== '@square/web-sdk'),
+  ...Object.keys(peerDependencies),
+];
+
+// Creating regexes of the packages to make sure subpaths of the
+// packages are also treated as external
+const regexesOfPackages = externalPackages.map((packageName) => new RegExp(`^${packageName}(/.*)?`));
+
 export default defineConfig({
   build: {
     lib: {
@@ -17,7 +27,25 @@ export default defineConfig({
       fileName: (ext) => `rswps.${ext}.js`,
     },
     rollupOptions: {
-      external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
+      external: regexesOfPackages,
+      output: [
+        {
+          dir: resolve(__dirname, 'dist'),
+          entryFileNames: '[name].cjs.js',
+          exports: 'named',
+          format: 'cjs',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+        {
+          dir: resolve(__dirname, 'dist'),
+          entryFileNames: '[name].es.js',
+          exports: 'named',
+          format: 'es',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+      ],
     },
     target: 'esnext',
     sourcemap: true,
